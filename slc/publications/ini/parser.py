dff6fd7466e66bec53513d1e0891c91086e88169
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-20 13:17:50 +0000

    added chapter export

--- slc/publications/ini/parser.py
+++ slc/publications/ini/parser.py
@@ -6,6 +6,8 @@ from zope import component
 from zope.formlib import form
 from zope.app.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
+from slc.publications.adapter import _get_storage_folder
+
 
 logger = logging.getLogger('slc.publications.ini')
 
@@ -13,6 +15,8 @@ class INIParser(object):
     """ parses metadata and chapters from ini files """
 
     interface.implements(IINIParser)
+    
+    ##### Parser
     
     def parse(self, ini):
         """ parses the given ini file and writes on the object """
@@ -70,10 +74,13 @@ class INIParser(object):
             L = getattr(F, chapter)
             _setMeta(TARGET=L, DATA=meta.items(section))
         
+    ######## Retriever        
         
+        
+       
         
     def retrieve(self, context):
-        """ """
+        """ retrieves the metadata from the object, all translations and all chapters """
         meta = ConfigParser.ConfigParser()
         meta.optionxform = str
         portal_languages = getToolByName(context, 'portal_languages')
@@ -85,14 +92,6 @@ class INIParser(object):
 
         # the main object
         canonical = context.getCanonical()
-        adapted = IPublication(canonical)
-        for attr in form_fields:
-            value = attr.field.get(adapted)
-            value = _vTs(value)
-            if not value: 
-                continue
-            meta.set('DEFAULT', attr.field.getName(), value)
-        
             
 
 
@@ -100,18 +99,23 @@ class INIParser(object):
         translations = canonical.getTranslations()
         for translation in translations.keys():
             if not translation:
-                continue
-                
-            meta.add_section(translation)      
-            adapted = IPublication(translations[translation][0])
+                lang = canonical.Language() or default_language
+            else:
+                lang = translation
+                            
+            t_ob = translations[translation][0]
+                    
+            meta.add_section(lang)      
+            adapted = IPublication(t_ob)
 
             for attr in form_fields:
                 value = attr.field.get(adapted)
                 value = _vTs(value)
                 if not value: 
                     continue
-                meta.set( translation, attr.field.getName(), value )
+                meta.set( lang, attr.field.getName(), value )
         
+            _retrieve_chapter_attrs(adapted, meta)
         
                  
         
@@ -122,7 +126,32 @@ class INIParser(object):
         meta.write(out)
         
         return out.getvalue()
-        
+       
+       
+       
+def _retrieve_chapter_attrs(ob, meta):
+    portal_languages = getToolByName(ob.context, 'portal_languages')
+    default_language = portal_languages.getDefaultLanguage()
+    suffix = ob.context.Language() or default_language
+
+    additionals = _get_storage_folder(ob.context)
+    chapters = ob.publication_data.get('chapters', [])
+                    
+    
+    for chapter in chapters:
+        section_name = "%s.%s" % (chapter, suffix)
+        link = getattr(additionals, chapter)
+        meta.add_section( section_name )   
+
+        schema = link.Schema()
+        for key in schema.keys():
+            value = getattr(link, schema[key].accessor)()           
+            value = _vTs(value)
+            if not value:
+                continue
+            meta.set(section_name, key, value )          
+            
+               
         
 def _vTs(value):
     """ parse a value into a string representation in one line """
