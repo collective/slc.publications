fe89760975a6340dc414fee646cdc1df21efbec6
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-28 12:18:35 +0000

    added utils and fixed the trnalsationmutator issue

---
+++ slc/publications/utils.py
@@ -0,0 +1,23 @@
+from Acquisition import aq_base, aq_inner, aq_parent
+
+
+def _get_storage_folder(ob):
+    """ Helper Method to fetch the folder containing additional material like chapters and further pdf parts 
+    """
+    additionals_id = ob.getId().replace('.pdf', '')+'_data'
+
+    if additionals_id == ob.getId():
+        raise AttributeError, "Cannot get a unique name for the additionals folder"
+        
+    container = aq_parent(aq_inner(ob)) 
+    
+    if additionals_id not in container.objectIds():
+        container.invokeFactory("Folder", additionals_id)
+        additionals = getattr(container, additionals_id)
+        additionals.setTitle('Additional material on %s' % ob.Title())
+        additionals.setExcludeFromNav(True)
+        additionals.reindexObject()
+    else:    
+        additionals = getattr(container, additionals_id)
+
+    return additionals
