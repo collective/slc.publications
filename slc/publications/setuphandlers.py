1833ca80e586eb600360e31d8b769a75beed77b8
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-01 11:59:24 +0000

    added action to types tool

---
+++ slc/publications/setuphandlers.py
@@ -0,0 +1,28 @@
+from zope.component import getUtility
+
+from Products.CMFCore.utils import getToolByName
+
+
+def setupActions(self):
+    # And now update the relevant portal_type actions
+    tool = getToolByName(self, 'portal_types')
+    filetype = getattr(tool, 'File')
+    acts = filter(lambda x: x.id == 'generate_metadata', filetype.listActions())
+    action = acts and acts[0] or None
+    if action is None:
+        filetype.addAction( 'generate_metadata',
+                         'Generate Metadata INI',
+                         'string:${object_url}/@@generate-metadata',
+                         '',
+                         'View',
+                         'object_actions',
+                         visible=1
+                       )
+                               
+def setupVarious(context):
+        
+    if context.readDataFile('publications-various.txt') is None:
+        return
+                
+    portal = context.getSite()
+    setupActions(portal)
