67616303c0f0f2dd77a695453aa052669ad1ce69
thomasw <thomasw@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-11-03 10:25:32 +0000

    bugfix: _get_storage_folder should only work on IPublicationEnhanced

--- slc/publications/utils.py
+++ slc/publications/utils.py
@@ -1,9 +1,13 @@
 from Acquisition import aq_base, aq_inner, aq_parent
+from slc.publications.interfaces import IPublicationEnhanced
 
 
 def _get_storage_folder(ob):
     """ Helper Method to fetch the folder containing additional material like chapters and further pdf parts 
     """
+    if not IPublicationEnhanced.providedBy(ob):
+        return None
+
     additionals_id = ob.getId().replace('.pdf', '')+'_data'
 
     if additionals_id == ob.getId():
