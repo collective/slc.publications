c5c798a4da4055765f2cf78a0c842ab6a68240ca
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-07-30 11:06:24 +0000

    Version bump (but still in dev - see setup.cfg). Add test_require products: plone.app.blob, PloneFlashUpload

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.4'
+version = '1.1.5'
 
 long_description = (
     read('README.txt')
@@ -30,7 +30,10 @@ long_description = (
     + '\n' 
     )
     
-tests_require=['zope.testing']
+tests_require=['zope.testing',
+               'plone.app.blob',
+               'Products.PloneFlashUpload',
+               ]
 
 setup(name='slc.publications',
       version=version,
