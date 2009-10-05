4169d08b0e59cc396373e85f15119720f9a35ae2
do3cc <do3cc@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-05 10:28:22 +0000

    Back to development: 1.1.9dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.8'
+version = '1.1.9dev'
 
 long_description = (
     read('README.txt')
@@ -70,6 +70,5 @@ setup(name='slc.publications',
       entry_points="""
       # -*- entry_points -*- 
       """,
-      paster_plugins = ["ZopeSkel"],
       )      
       
