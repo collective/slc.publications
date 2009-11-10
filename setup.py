60adf071d1c9d315cc93238e2906f838d7f1df50
thomasw <thomasw@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-10 09:05:36 +0000

    Back to development: 1.1.14dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.13'
+version = '1.1.14dev'
 
 long_description = (
     read('README.txt')
