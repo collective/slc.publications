28586ae116e07f08e4e9b18eb87b2d014e4a7082
pilz <pilz@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-01-06 21:24:06 +0000

    pep8 cleanup

--- slc/publications/utils.py
+++ slc/publications/utils.py
@@ -1,26 +1,29 @@
-from Acquisition import aq_base, aq_inner, aq_parent
+from Acquisition import aq_inner, aq_parent
 from slc.publications.interfaces import IPublicationEnhanced
 
+
 def _get_storage_folder(ob):
-    """ Helper Method to fetch the folder containing additional material like chapters and further pdf parts 
+    """ Helper Method to fetch the folder containing additional material
+        like chapters and further pdf parts
     """
     if not IPublicationEnhanced.providedBy(ob):
         return None
 
-    additionals_id = ob.getId().replace('.pdf', '')+'_data'
+    additionals_id = ob.getId().replace('.pdf', '') + '_data'
 
     if additionals_id == ob.getId():
-        raise AttributeError, "Cannot get a unique name for the additionals folder"
-        
-    container = aq_parent(aq_inner(ob)) 
-    
+        raise AttributeError("Cannot get a unique name" + \
+            " for the additionals folder")
+
+    container = aq_parent(aq_inner(ob))
+
     if additionals_id not in container.objectIds():
         container.invokeFactory("Folder", additionals_id)
         additionals = getattr(container, additionals_id)
         additionals.setTitle('Additional material on %s' % ob.Title())
         additionals.setExcludeFromNav(True)
         additionals.reindexObject()
-    else:    
+    else:
         additionals = getattr(container, additionals_id)
 
     return additionals
