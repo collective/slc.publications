119cd48c6d52f4071ee577308aed6c1e415dafa5
cillian <cillian@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-03 11:49:20 +0000

    Preparing for a bugfix release 1.1.15

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.15dev'
+version = '1.1.15'
 
 long_description = (
     read('README.txt')
