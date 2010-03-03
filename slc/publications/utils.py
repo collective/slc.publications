fa870a2119b5297e15aa11c28ba7e797332ac0ab
cillian <cillian@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-03 10:33:33 +0000

    Bypassing security checks when adding the publication data folder. This allows the data folder to be created when an Anonymous user views the publication.

--- slc/publications/utils.py
+++ slc/publications/utils.py
@@ -1,6 +1,11 @@
 from AccessControl import Unauthorized
 from Acquisition import aq_inner, aq_parent
 
+from zope.component import getUtility
+from zope.component.interfaces import IFactory
+
+from Products.CMFCore.utils import getToolByName
+
 from slc.publications.interfaces import IPublicationEnhanced
 
 
@@ -20,20 +25,16 @@ def _get_storage_folder(ob):
     container = aq_parent(aq_inner(ob))
 
     if additionals_id not in container.objectIds():
-        try:
-            # FIXME: shouldn't this additional data be created along
-            # with the pdf?
-
-            # Calling this from browser/publications.py e.g. viewing a
-            # publication as an Anonymous user was returning
-            # Unauthorized.
-            container.invokeFactory("Folder", additionals_id)
-            additionals = getattr(container, additionals_id)
-            additionals.setTitle('Additional material on %s' % ob.Title())
-            additionals.setExcludeFromNav(True)
-            additionals.reindexObject()
-        except Unauthorized:
-            return None
+        #container.invokeFactory("Folder", additionals_id)
+        pt = getToolByName(ob, 'portal_types')
+        folder_type = pt.getTypeInfo("Folder")
+        factory = getUtility(IFactory, folder_type.factory)
+        data_obj = factory(additionals_id, *args, **kw)
+        container._setObject(additionals_id, data_obj)
+        additionals = getattr(container, additionals_id)
+        additionals.title = 'Additional material on %s' % ob.Title()
+        additionals.setExcludeFromNav(True)
+        additionals.reindexObject()
     else:
         additionals = getattr(container, additionals_id)
 
