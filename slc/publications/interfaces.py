4435bb71cb59302acd501ef6f4a6d8a19ef700ff
cillian <cillian@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-12-13 15:06:22 +0000

    Tidy up

--- slc/publications/interfaces.py
+++ slc/publications/interfaces.py
@@ -6,7 +6,7 @@ from zope.app.content import interfaces as contentifaces
 class IAnyPublicationCapable(Interface):
     """Any aspect of publication/content capable.
     """
-    
+
 class IPossiblePublication(IAnyPublicationCapable):
     """ All objects that should have the ability to be converted to some
         form of publication should implement this interface.
@@ -20,7 +20,7 @@ class IPossiblePublicationContainer(IAnyPublicationCapable):
 class IPublicationEnhanced(Interface):
     """ Marker interface for publications
     """
-        
+
 class IPublicationContainerEnhanced(Interface):
     """ Any folderish entity that has had it's IPublicationContainer features
         enabled.
@@ -32,31 +32,35 @@ alsoProvides(IPublicationEnhanced, contentifaces.IContentType)
 class IPublication(Interface):
     """ Objects which have publication information.
     """
-    
+
 class IPublicationProvider(Interface):
     """ Provide publications.
     """
-    publication_items = schema.List(title=u'Publication Items', required=True, readonly=True)    
+    publication_items = schema.List(
+        title=u'Publication Items', required=True, readonly=True)
 
 
-                                    
+
 class IBasicPublicationSupport(Interface):
     """ Provides certain information about publication support.
     """
-    support_enabled = schema.Bool(title=u'Publication Support Enabled?', required=True, readonly=True)                
+    support_enabled = schema.Bool(
+        title=u'Publication Support Enabled?', required=True, readonly=True)
 
 class IPublicationSupport(IBasicPublicationSupport):
     """ Provides full information about publication support.
-    """     
-                                      
+    """
+
 class IMediaActivator(Interface):
     """ For seeing the activation status or toggling activation.
     """
-    media_activated = schema.Bool(title=u'Media Activated',required=True, readonly=False)
-    
-    
+    media_activated = schema.Bool(
+        title=u'Media Activated',required=True, readonly=False)
+
+
 class IPrettyFormatter(Interface):
     """ Provides functionality to format strings """
 
 class IAdditionalPublicationInfo(Interface):
-    """ Marker for adapter that provides additional info in the publication view """
+    """ Marker for adapter that provides additional info in the
+    publication view """
