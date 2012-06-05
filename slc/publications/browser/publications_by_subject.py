dc078ca4685b5c5e0017858ddc89fdd9f73ad4f7
zupo <zupo@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-06-05 08:23:24 +0000

    remove usage of AdvancedQuery from publications_by_subject.py, add tests for it, pep8 & pyflakes cleanup

--- slc/publications/browser/publications_by_subject.py
+++ slc/publications/browser/publications_by_subject.py
@@ -1,45 +1,56 @@
-import Acquisition, os
-from types import *
+ # -*- coding: utf-8 -*-
+
+import Acquisition
+import os
+from types import ListType
+from types import TupleType
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from Products.AdvancedQuery import In, Eq, Le, Ge, And, Or
-
 
 
 class PublicationsBySubjectView(BrowserView):
-    """View for displaying the publications by Subject overview page 
+    """View for displaying the publications by Subject overview page
     """
     template = ViewPageTemplateFile('templates/publications_by_subject.pt')
-    
+
     def __call__(self):
         """ call - XXX: we should think about caching here..."""
         self.request.set('disable_border', True)
-        
+
         context = Acquisition.aq_inner(self.context)
         subject = self.request.get('subject', [])
-        
+
         portal_languages = getToolByName(context, 'portal_languages')
         preflang = portal_languages.getPreferredLanguage()
-        
+
         portal_catalog = getToolByName(context, 'portal_catalog')
         if hasattr(portal_catalog, 'getZCatalog'):
             portal_catalog = portal_catalog.getZCatalog()
-        
-        PQ = Eq('portal_type', 'File') & \
-             In('object_provides', 'slc.publications.interfaces.IPublicationEnhanced') & \
-             In('Subject', subject) & \
-             Eq('review_state', 'published') & \
-             In('Language', [preflang, ''])
-             
-        PUBS = portal_catalog.evalAdvancedQuery(PQ, (('effective','desc'),) )
-        
-       
+
+        # PQ = Eq('portal_type', 'File') & \
+        #      In('object_provides', 'slc.publications.interfaces.IPublicationEnhanced') & \
+        #      In('Subject', subject) & \
+        #      Eq('review_state', 'published') & \
+        #      In('Language', [preflang, ''])
+        # PUBS = portal_catalog.evalAdvancedQuery(PQ, (('getObjectPositionInParent','desc'),) )
+
+        query = {
+            'portal_type': 'File',
+            'object_provides': 'slc.publications.interfaces.IPublicationEnhanced',
+            'Subject': subject,
+            'review_state': 'published',
+            'Language': [preflang, ''],
+            'sort_on': 'getObjPositionInParent',
+            'sort_order': 'ascending',
+        }
+        PUBS = portal_catalog(query)
+
         publist = {}
         parentlist = []
-        
+
         # we implement a simple sorting by folder id to have a grouping. This is not too nice yet
-            
+
         for P in PUBS:
             path = P.getPath()
             parentpath = os.path.dirname(path)
@@ -47,29 +58,35 @@ class PublicationsBySubjectView(BrowserView):
             section = publist.get(parentpath, [])
             section.append(P)
             publist[parentpath] = section
-            
-        Q = In('portal_type', ['Folder', 'Large Plone Folder']) & \
-            In('object_provides', 'slc.publications.interfaces.IPublicationContainerEnhanced')& \
-            In('Language', [preflang, ''])
-        PARENTS = portal_catalog.evalAdvancedQuery(Q, (('getObjPositionInParent','asc'),) )
-        
+
+        # Q = In('portal_type', ['Folder', 'Large Plone Folder']) & \
+        #     In('object_provides', 'slc.publications.interfaces.IPublicationContainerEnhanced')& \
+        #     In('Language', [preflang, ''])
+        # PARENTS = portal_catalog.evalAdvancedQuery(Q, (('getObjPositionInParent','asc'),) )
+
+        query = {
+            'portal_type': ['Folder', 'Large Plone Folder'],
+            'object_provides': 'slc.publications.interfaces.IPublicationContainerEnhanced',
+            'Language': [preflang, ''],
+            'sort_on': 'getObjPositionInParent',
+            'sort_order': 'ascending',
+        }
+        PARENTS = portal_catalog(query)
+
         self.publist = publist
         self.parents = [(x.getPath(), x) for x in PARENTS]
-              
-        return self.template() 
-        
-        
-        
+
+        return self.template()
+
     def subject(self):
         """ return the list of subjects """
         subject = self.request.get('subject', [])
         if type(subject) not in [ListType, TupleType]:
             subject = [subject]
-            
+
         # XXX: implement keyword translation here
         return ", ".join([x for x in subject])
-        
-        
+
     def subject_label(self):
         """ return the list of pretty subjects """
         subject_label = self.request.get('subject_label', [])
---
+++ slc/publications/tests/test_publications_by_subject.py
@@ -0,0 +1,103 @@
+ # -*- coding: utf-8 -*-
+"""Tests for slc.publication PublicationsBySubjectView."""
+
+from p4a.subtyper.interfaces import ISubtyper
+from slc.publications.interfaces import IPublicationContainerEnhanced
+from slc.publications.interfaces import IPublicationEnhanced
+from slc.publications.tests.base import IntegrationTestCase
+from zope.component import getUtility
+from zope.interface import alsoProvides
+
+import unittest2 as unittest
+
+
+class TestPublicationsBySubjectView(IntegrationTestCase):
+    """Test for the @@file_view."""
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        self.catalog = self.portal.portal_catalog
+        self.workflow = self.portal.portal_workflow
+
+        # @@file_view expects to find File content types in 'publish' state,
+        # however, by default Files don't have any workflow assigned to them
+        self.workflow.setChainForPortalTypes(['File'], 'plone_workflow')
+
+        # create a folder that will hold publications and change it's subtype
+        # to PublicationContainer
+        self.portal.invokeFactory('Folder', 'pubs1', title='Publications 1')
+        self.portal.invokeFactory('Folder', 'pubs2', title='Publications 2')
+        self.pubs1 = self.portal.pubs1
+        self.pubs2 = self.portal.pubs2
+        alsoProvides(self.pubs1, IPublicationContainerEnhanced)
+        alsoProvides(self.pubs2, IPublicationContainerEnhanced)
+        self.subtyper = getUtility(ISubtyper)
+        self.subtyper.change_type(self.pubs1, u'slc.publications.FolderPublicationContainer')
+        self.subtyper.change_type(self.pubs2, u'slc.publications.FolderPublicationContainer')
+
+        # add some publications
+        self.pubs1.invokeFactory('File', 'pub1')
+        self.pubs1.invokeFactory('File', 'pub2')
+        self.pubs2.invokeFactory('File', 'pub3')
+
+        # set Subjects
+        self.pubs1.pub1.setSubject(['foo', 'bar'])
+        self.pubs1.pub2.setSubject(['foo', ])
+        self.pubs2.pub3.setSubject(['bar', ])
+
+        # publish publications
+        self.workflow.doActionFor(self.portal.pubs1['pub1'], 'publish')
+        self.workflow.doActionFor(self.portal.pubs1['pub2'], 'publish')
+        self.workflow.doActionFor(self.portal.pubs2['pub3'], 'publish')
+
+        # reindex everything to update the catalog
+        self.catalog.clearFindAndRebuild()
+
+    def test_subtyping(self):
+        """Confirm that Folder and File are correctly subtyped."""
+        from p4a.subtyper.interfaces import ISubtyper
+        self.subtyper = getUtility(ISubtyper)
+
+        # Folder
+        self.assertTrue(IPublicationContainerEnhanced.providedBy(self.pubs1))
+        self.assertEquals(
+            self.subtyper.existing_type(self.pubs1).name,
+            u'slc.publications.FolderPublicationContainer'
+        )
+
+        # File
+        self.assertTrue(IPublicationEnhanced.providedBy(self.pubs1.pub1))
+        self.assertEquals(
+            self.subtyper.existing_type(self.pubs1.pub1).name,
+            u'slc.publications.Publication'
+        )
+
+    def test_publist_and_parents(self):
+        """Simple test of what self.publist and self.parents contain when
+        view is called.
+        """
+        self.request['subject'] = ['foo', 'bar']
+        view = self.portal.unrestrictedTraverse('@@publications-by-subject')
+        view()
+
+        publist = view.publist
+        self.assertEquals(publist.keys(), ['/plone/pubs1', '/plone/pubs2'])
+        self.assertEquals(publist['/plone/pubs1'][0].id, 'pub1')
+        self.assertEquals(publist['/plone/pubs1'][1].id, 'pub2')
+        self.assertEquals(publist['/plone/pubs2'][0].id, 'pub3')
+
+        parents = view.parents
+        self.assertEquals(len(parents), 2)
+        self.assertEquals(parents[0][0], '/plone/pubs1')
+        self.assertEquals(parents[0][1].id, 'pubs1')
+        self.assertEquals(parents[1][0], '/plone/pubs2')
+        self.assertEquals(parents[1][1].id, 'pubs2')
+
+
+def test_suite():
+    """This sets up a test suite that actually runs the tests in
+    the class above.
+    """
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
