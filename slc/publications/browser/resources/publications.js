91e1a559997d29b8be9e48a3aeccee30b3b0fe53
cillian <cillian@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-10-24 13:43:55 +0000

    Publications revamp refs #3790: Register the relevant JavaScript and CSS for the publications_view
    git-svn-id: https://svn.plone.org/svn/collective/slc.publications/branches/plone4-compatible@251311 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ slc/publications/browser/resources/publications.js
@@ -0,0 +1,43 @@
+var PUBS = {};
+
+PUBS.queryPublications = function () {
+    var query = jQuery("#publicationsFilter").serialize();
+    jQuery.getJSON(
+        "publications_view.json?" + query, function (data) {
+            var items = [],
+            results = "";
+            jQuery.each(data, function (key, val) {
+                var link = "<a href='" + val.path + "'>" + val.title + "</a>";
+                items.push('<tr><td>' + link + '</td><td>' +
+                           val.effective_date + '</td><td>' +
+                           val.type + '</td><td><em class="discrete size">' +
+                           val.size + '</em></td></tr>');
+            });
+            results = jQuery('<tbody/>', {html: items.join('')});
+            jQuery("#resultTable tbody").replaceWith(results);
+        });
+};
+
+jQuery(document).ready(function() {
+    debugger;
+    jQuery("#publicationsFilter :input").change(function () {
+        PUBS.queryPublications();
+        jQuery("#show-all").remove();
+        return false;
+    });
+    jQuery("#show-all").click(function () {
+        jQuery("#publicationsFilter")
+            .append("<input type='hidden' name='show-all' value='True'/>")
+        PUBS.queryPublications();
+        jQuery("#show-all").remove();
+        return false;
+    });
+    jQuery("input[name=SearchableText]").keyup(function () {
+        PUBS.queryPublications();
+    });
+
+    // Don't warn the user that they have already submitted the form
+    jQuery("#queryPublications").unbind('click');
+
+    var recentcarousel = jQuery(".recentcarousel").jcarousel({scroll: 1});
+});
