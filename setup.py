719b0cb839bc02751ade3641e85aa28d588616aa
Cillian de Roiste <deroiste@syslab.com>
2013-02-20 16:28:34 +0000

    Back to development: 4.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.5'
+version = '4.6.dev0'
 
 long_description = (
     read('README.rst')
