4e6a4c49ce63b099e4ea0b98a7cdf7ebbd7548ec
jcerjak <jcerjak@termitnjak.si>
2013-02-25 15:52:10 +0100

    remove methods that generate multiple carousel images
    This was needed before, when we had multiple images per publication in the carousel, now we just use the publication cover image. There is a branch multiple-carousel-images if we ever need to re-introduce this functionality.

--- slc/publications/browser/publications.py
+++ slc/publications/browser/publications.py
@@ -3,20 +3,13 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from ordereddict import OrderedDict
-from slc.publications import HAVE_LINGUAPLONE
-from slc.publications import SLCPublicationsMessageFactory as _
 from slc.publications.config import PUB_TYPES
-from slc.publications.utils import _get_storage_folder
 from zope.app.component.hooks import getSite
 from zope.i18n import translate
 
 import json
 import locale
 import logging
-import os
-import subprocess
-import tempfile
-
 
 logger = logging.getLogger('slc.publications.publications.publications.py')
 
@@ -30,8 +23,6 @@ class PublicationsView(BrowserView):
     """ Filter/Search for Publications """
 
     template = ViewPageTemplateFile('templates/publications.pt')
-    #template.id = "publications-view"
-
     has_more_results = False
 
     def __init__(self, context, request):
@@ -123,104 +114,10 @@ class PublicationsView(BrowserView):
             pub_details.append({
                 "absolute_url": pub.absolute_url(),
                 "title": pub.Title(),
-                "images": [],
                 "description": pub.Description(),
-                "images": self._get_carousel_images(pub),
                 "date": pub.Date()
             })
         return pub_details
-
-    def _get_carousel_images(self, pub):
-        pub_folder          = _get_storage_folder(pub)
-        pub_folder_contents = pub_folder.objectIds()
-
-        if "carousel_1.gif" not in pub_folder_contents:
-            self._generate_carousel_images(pub, pub_folder)
-
-        images = [
-            i for i in pub_folder_contents
-            if i.startswith("carousel_")]
-        return images
-
-    def _generate_carousel_images(self, pub, pub_folder):
-        pub_folder = _get_storage_folder(pub)
-        tmp_pdf_in = tmp_dir = None
-        status     = 1
-
-        try:
-            if HAVE_LINGUAPLONE:
-                mainpub = pub.getCanonical()
-            else:
-                mainpub = pub
-
-            data = str(mainpub.getFile())
-            if not data:
-                return 0
-
-            fd, tmp_pdf_in = tempfile.mkstemp(suffix='.pdf')
-            os.close(fd)
-            tmp_dir = tempfile.mkdtemp()
-
-            fhout = open(tmp_pdf_in, "w")
-            fhout.write(data)
-            fhout.seek(0)
-
-            # There isn't a nice way to only split the first 3 pages
-            split_pdf_cmd = "pdftk %s burst output %s/%%04d.pdf" % (
-                tmp_pdf_in, tmp_dir)
-            self._run_shell_command(split_pdf_cmd)
-
-            pdfs_to_images_cmd = (
-                "convert %s/000[1,2,3].pdf -resize 180x255 %s/%%04d.gif" % (
-                    tmp_dir, tmp_dir))
-            self._run_shell_command(pdfs_to_images_cmd)
-
-            imgfiles = [
-                gif for gif in os.listdir(tmp_dir)
-                if os.path.splitext(gif)[1] == '.gif']
-            imgfiles.sort()
-
-            for i, img in enumerate(imgfiles):
-                img = open(os.path.join(tmp_dir, img), "r")
-                img_data = img.read()
-                img.close()
-
-                pub_folder.invokeFactory(
-                    "Image", "carousel_%s.gif" % i, image=img_data)
-
-        except Exception, e:
-            logger.warn("generateImage: Could not autoconvert because: %s" % e)
-            status = 0
-
-        # try to clean up
-        if tmp_pdf_in is not None:
-            try:
-                os.remove(tmp_pdf_in)
-            except:
-                pass
-        if tmp_dir is not None:
-            try:
-                for i in os.listdir(tmp_dir):
-                    path = os.path.join(tmp_dir, i)
-                    print "removing %s" % path
-                    os.remove(path)
-                os.removedirs(tmp_dir)
-            except:
-                pass
-
-        return status
-
-    def _run_shell_command(self, cmd):
-        logger.info("Running command: %s" % cmd)
-        try:
-            process = subprocess.Popen(
-                cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            process.communicate()
-        except Exception, e:
-            logger.error("Failed to run command: %s\n Exception: %s" % (
-                    cmd, e))
-        if process.returncode == 1:
-            logger.warn("Failed to run command: %s" % cmd)
 
     def get_publication_type(self, path):
         """The publication type is determined by the folder"""
